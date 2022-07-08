1720429d3fd06fe44a850f4e47a44e2b081863d2
Mostafa-Bkry <mbkry661@gmail.com>
2022-07-08 18:53:35 +0200

    basic style of app

---
+++ lib/layout/home_screen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/modules/archived_tasks_screen.dart';
+import 'package:todo_app/modules/done_tasks_screen.dart';
+import 'package:todo_app/modules/new_tasks_screen.dart';
+
+class HomeLayout extends StatefulWidget {
+  const HomeLayout({Key? key}) : super(key: key);
+
+  @override
+  State<HomeLayout> createState() => _HomeLayoutState();
+}
+
+class _HomeLayoutState extends State<HomeLayout> {
+  int index = 0;
+  List<Widget> screens = const [
+    NewTaskScreen(),
+    DoneTasksScreen(),
+    ArchivedTasksScreen(),
+  ];
+
+  List<String> titles = ['New Tasks', 'Done Tasks', 'Archived Tasks'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          titles[index],
+        ),
+      ),
+      body: screens[index],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          print(getName());
+          String name = await getName();
+          print(name);
+        },
+        child: const Icon(
+          Icons.add,
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 20.0,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.task),
+            label: 'Tasks',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.check_circle_outline_rounded),
+            label: 'Done',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.archive_rounded),
+            label: 'Archived',
+          ),
+        ],
+        currentIndex: index,
+        onTap: (index) {
+          setState(() {
+            this.index = index;
+          });
+        },
+      ),
+    );
+  }
+}
+
+Future<String> getName() async {
+  return 'Ahmed Mohamed';
+}
---
+++ lib/modules/archived_tasks_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ArchivedTasksScreen extends StatefulWidget {
+  const ArchivedTasksScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ArchivedTasksScreen> createState() => _ArchivedTasksScreenState();
+}
+
+class _ArchivedTasksScreenState extends State<ArchivedTasksScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Archived Tasks',
+        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
